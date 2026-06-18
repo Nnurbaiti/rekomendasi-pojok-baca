1ae9ebf7f1ce0fcc7e0b67da0770bd272bb7fd2c
Nurbaiti Nasiroh <154042355+Nnurbaiti@users.noreply.github.com>
2026-06-19 01:22:57 +0700

    fixed 1st

--- main.py
+++ main.py
@@ -283,9 +283,9 @@ def load_books():
     books = pd.DataFrame(response.json())
 
     books["combined"] = (
-        (books["subcategory"].astype(str) + " ") * 4 +
-        (books["title"].astype(str) + " ") * 3 +
-        (books["author"].astype(str) + " ") * 2 +
+        (books["subcategory"].astype(str) + " ") * 3 +
+        (books["title"].astype(str) + " ") * 2 +
+        (books["author"].astype(str) + " ") * 1 +
         (books["category"].astype(str) + " ") * 1 + 
         (books["sinopsis"].astype(str) + " ") * 1
     )
@@ -486,7 +486,7 @@ def recommend():
     )
 
     user_text = (
-        (" ".join(preferred_subcategories) + " ") * 5 +
+        (" ".join(preferred_subcategories) + " ") * 3 +
         
         #judul, penulis, kategori, sub kategori
         (survey_favorite_text + " ") * 2 + 
