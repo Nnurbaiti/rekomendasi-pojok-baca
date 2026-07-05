5d6a4b249a97a7b23b4b329506e0c9e9c0e23c5a
Nurbaiti Nasiroh <154042355+Nnurbaiti@users.noreply.github.com>
2026-07-06 04:44:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -479,8 +479,8 @@ def load_books():
     books = pd.DataFrame(response.json())
 
     books["combined"] = (
-        (books["title"].astype(str) + " ") * 3 +
-        (books["author"].astype(str) + " ") * 2 +
+        (books["title"].astype(str) + " ") * 1 +
+        (books["author"].astype(str) + " ") * 1 +
         (books["category"].astype(str) + " ") * 1 +
         (books["subcategory"].astype(str) + " ") * 1 +
         (books["sinopsis"].astype(str) + " ") * 1
