58a5da40cb833f6a18d3fb4f5e962bd9b7ee1581
Nurbaiti Nasiroh <154042355+Nnurbaiti@users.noreply.github.com>
2026-08-01 18:11:16 +0700

    semua sinopsis

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def load_books():
         (books["subcategory"].astype(str) + " ") +
         (books["title"].astype(str) + " ") +
         (books["author"].astype(str) + " ") +        
-        # (books["sinopsis"].astype(str) + " ") +  
+        (books["sinopsis"].astype(str) + " ") +  
         (books["category"].astype(str) + " ") 
     )
 
